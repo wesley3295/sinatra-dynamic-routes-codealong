2d2a09a5a39342bfd582c15c53a9b7c2e8b57539
Wesley <wspencer3295@yahoo.com>
2020-12-15 13:33:39 -0700

    commit tree 7b908988680a404ebc4ba39cc0e55eed202025f9

--- app.rb
+++ app.rb
@@ -12,6 +12,19 @@ class App < Sinatra::Base
     @user_name = params[:name]
     "Hello #{@user_name}!"
   end
+  
+  get "/goodbye/:name" do
+    @name = params[:name]
+    "Goodbye, #{@name}."
+  end
+  
+  get "/multiply/:num1/:num2" do
+    @equals = params[:num1].to_i * params[:num2].to_i
+    "#{@equals}"
+    # @num1 = params[:num1]
+    # @num2 = params[:num2]
+    # @equals= @num1.to_i * @num2.to_i
+  end
 
   # Code your final two routes here:
 
